96e0f52a5278c3cc4a59994f718433080d2ba1c4
Debashis Roy <goodquote39@hotmail.com>
2024-02-06 14:24:40 +0600

    flowbite added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
       <h1>ফাতেমাতুজ জোহরা মহিলা মাদরাসা</h1>
 
       <h2>Coming Soon</h2>
+      <button
+        type="button"
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+      >
+        Dark
+      </button>
     </>
   );
 }
