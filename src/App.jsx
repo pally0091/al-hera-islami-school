b9be92134ca8ea2eb59966c276f396c2cc550fc4
Debashis Roy (Esa) <108608342+pally0091@users.noreply.github.com>
2024-09-29 19:53:48 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>ফাতেমাতুজ জোহরা মহিলা মাদরাসা</h1>
+      <h1>Al-Hera-Islamic-School</h1>
 
       <h2>Coming Soon</h2>
       <button
