a26e127906448444efd46c6f7a786ed8158ed28f
Debashis Roy <goodquote39@hotmail.com>
2024-09-29 22:30:50 +0600

    Updated index.html, App.css, App.jsx, and index.css files with changes to title, meta title, CSS styles, and font imports.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Al-Hera-Islamic-School</h1>
+      <h1>Al Hera International Madrasah</h1>
 
-      <h2>Coming Soon</h2>
-      <button
-        type="button"
-        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-      >
-        Dark
-      </button>
+      <h6>Coming Soon</h6>
     </>
   );
 }
