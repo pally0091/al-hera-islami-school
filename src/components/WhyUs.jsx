0492edf6e3a92791862b187bada9971309a2d4b4
Debashis Roy <goodquote39@hotmail.com>
2024-10-04 11:42:53 +0600

    Refactored WhyChooseUs component to use data from external file and map over it to render list of items, replacing hardcoded HTML elements.

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -1,39 +1,19 @@
+import { data } from "../data/whyus";
+
 const WhyChooseUs = () => {
   return (
     <section>
       <h2 className="text-center mb-8">কেন আমাদের বেছে নিবেন?</h2>
       <div className="grid gap-8 grid-cols-1 md:grid-cols-2 max-w-4xl mx-auto">
-        <div className="border border-accent p-4 hover:shadow-md hover:shadow-accent transition-all duration-500 cursor-default bg-white rounded-sm">
-          <h4 className="font-semibold mb-4">সর্বাঙ্গীণ শিক্ষা</h4>
-          <p className="leading-relaxed">
-            আমাদের মাদ্রাসা আধুনিক শিক্ষা ও ধর্মীয় শিক্ষার সমন্বয়ে একটি
-            পূর্ণাঙ্গ শিক্ষা ব্যবস্থা প্রদান করে। শিক্ষার্থীদের জাগতিক ও
-            আধ্যাত্মিক জ্ঞানার্জনে সহায়তা করা আমাদের মূল লক্ষ্য।
-          </p>
-        </div>
-        <div className="border border-accent p-4 hover:shadow-md hover:shadow-accent transition-all duration- cursor-default bg-white rounded-sm">
-          <h4 className="font-semibold mb-4">অভিজ্ঞ শিক্ষকবৃন্দ</h4>
-          <p className="leading-relaxed">
-            আমাদের শিক্ষকমণ্ডলী উচ্চ শিক্ষিত ও অভিজ্ঞ, যারা শিক্ষার্থীদের
-            ব্যক্তিগত যত্ন নিয়ে দিকনির্দেশনা দেন। তারা প্রতিটি শিক্ষার্থীর
-            উন্নতি নিশ্চিত করতে নিবেদিত।
-          </p>
-        </div>
-        <div className="border border-accent p-4 hover:shadow-md hover:shadow-accent transition-all duration- cursor-default bg-white rounded-sm">
-          <h4 className="font-semibold mb-4">নৈতিক ও আধ্যাত্মিক বিকাশ</h4>
-          <p className="leading-relaxed">
-            আমরা শিক্ষার্থীদের নৈতিকতা ও আধ্যাত্মিক উন্নতির উপর বিশেষ গুরুত্ব
-            দেই, যাতে তারা শুধু শিক্ষাগত উৎকর্ষতা নয়, বরং জীবনের সকল ক্ষেত্রে
-            আদর্শ মানুষ হয়ে উঠতে পারে।
-          </p>
-        </div>
-        <div className="border border-accent p-4 hover:shadow-md hover:shadow-accent transition-all duration- cursor-default bg-white rounded-sm">
-          <h4 className="font-semibold mb-4">সুন্দর শিক্ষাবান্ধব পরিবেশ</h4>
-          <p className="leading-relaxed">
-            শিক্ষার্থীদের জন্য একটি সুরক্ষিত, শান্তিপূর্ণ এবং শিক্ষাবান্ধব
-            পরিবেশ তৈরি করা হয়েছে, যা তাদের শারীরিক ও মানসিক বিকাশে সহায়ক।
-          </p>
-        </div>
+        {data.map((r, index) => (
+          <div
+            key={index}
+            className="border border-accent p-4 hover:shadow-md hover:shadow-accent transition-all duration-500 cursor-default bg-white rounded-sm"
+          >
+            <h4 className="font-semibold mb-4">{r.title}</h4>
+            <p className="leading-relaxed">{r.detail}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
