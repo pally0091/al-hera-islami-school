d6fa5cd907a9ae50560defede59024ead435b54a
Debashis Roy <goodquote39@hotmail.com>
2024-10-23 11:48:20 +0600

    events & testimonials set to dynamically

---
+++ js/events.js
@@ -0,0 +1,44 @@
+const events = [
+  {
+    date: "04 Dec 2024",
+    title: "Annual Exam",
+    description:
+      "The Annual Exam at Al Hera International School & Madrasah helps students show their progress and achievements from the year. We encourage all students to prepare well and do their best.",
+  },
+  {
+    date: "15 Dec 2024",
+    title: "Annual Exam Result Publish",
+    description:
+      "The Annual Exam results will be published at Al Hera International School & Madrasah. We celebrate the hard work and progress of our students. Best wishes to all!",
+  },
+];
+
+// Get the container where the events will be injected
+const eventsContainer = document.getElementById("events-container");
+
+// Function to create the event HTML dynamically
+function renderEvents() {
+  events.forEach((event) => {
+    // Create the HTML structure for each event
+    const eventHTML = `
+        <div class="row g-4 event-item">
+          <div class="col-3 col-lg-2 pe-0">
+            <div class="text-center border-bottom border-dark py-3 px-2">
+              <h6>${event.date}</h6>
+            </div>
+          </div>
+          <div class="col-9 col-lg-6 border-start border-dark pb-5">
+            <div class="ms-3">
+              <h4 class="mb-3">${event.title}</h4>
+              <p class="mb-4">${event.description}</p>
+            </div>
+          </div>
+        </div>`;
+
+    // Append the event HTML to the container
+    eventsContainer.innerHTML += eventHTML;
+  });
+}
+
+// Call the function to render the events on page load
+renderEvents();
---
+++ js/testimonial.js
@@ -0,0 +1,42 @@
+const testimonials = [
+  {
+    name: "Lina Amir",
+    relation: "Mother of Ahmed Mujnabin",
+    feedback:
+      "I'm really impressed with Al Hera International School and Madrasa. I like how we get the lesson plans all at once and the kids can still learn even when they're sick. They're learning English speaking so well! And our principal, Md. Ali Azam, is so nice and helpful.",
+  },
+  {
+    name: "Fatema Begum",
+    relation: "Mother of Ahmed Bin Kabir",
+    feedback:
+      "I'm very happy with my decision to enroll my twin sons in Al Hera International School and Madrasa. They are doing very well in all subjects and the teachers are excellent. I highly recommend this school.",
+  },
+];
+
+// Get the container where the testimonials will be injected
+const testimonialCarousel = document.getElementById("testimonial-carousel");
+
+// Function to create the testimonial HTML dynamically
+function renderTestimonials() {
+  testimonials.forEach((testimonial) => {
+    // Create the HTML structure for each testimonial
+    const testimonialHTML = `
+        <div class="testimonial-item">
+          <div class="d-flex mb-3">
+            <div class="ps-3 my-auto">
+              <h5 class="mb-0">${testimonial.name}</h5>
+              <p class="m-0">${testimonial.relation}</p>
+            </div>
+          </div>
+          <div class="testimonial-content">
+            <p class="fs-5 m-0 pt-3">${testimonial.feedback}</p>
+          </div>
+        </div>`;
+
+    // Append the testimonial HTML to the carousel
+    testimonialCarousel.innerHTML += testimonialHTML;
+  });
+}
+
+// Call the function to render the testimonials on page load
+renderTestimonials();
