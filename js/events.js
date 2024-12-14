6560498195a6d97229ce41f0df39cec700ffc527
F Rabbi <goodquote39@hotmail.com>
2024-12-14 20:22:00 +0600

    events deleted

--- js/events.js
+++ js/events.js
@@ -1,17 +1,5 @@
 const events = [
   {
-    date: "04 Dec 2024",
-    title: "Annual Exam",
-    description:
-      "The Annual Exam at Al Hera International School & Madrasah helps students show their progress and achievements from the year. We encourage all students to prepare well and do their best.",
-  },
-  {
-    date: "15 Dec 2024",
-    title: "Annual Exam Result Publish",
-    description:
-      "The Annual Exam results will be published at Al Hera International School & Madrasah. We celebrate the hard work and progress of our students. Best wishes to all!",
-  },
-  {
     date: "1 Jan 2025",
     title: "New Campus Grand Opening",
     description:
