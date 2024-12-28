b572047f859a246c50d16bc50a3fd8012a9e50a6
F Rabbi <goodquote39@hotmail.com>
2024-12-28 11:05:30 +0600

    banner section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+const Banner = () => {
+  return (
+    <section>
+      <h1 className="text-2xl">This is Banner</h1>
+      <p>Home Page</p>
+    </section>
+  );
+};
+
 const Home = () => {
   return (
     <div>
-      <h1>This is Home page</h1>
+      <Banner />
     </div>
   );
 };
